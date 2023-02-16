2f929f0b496a36a0e10d022cb183e1369727aa62
muulinha <lucasfl.araujo@hotmail.com>
2023-02-16 19:17:12 +0800

    README file completed

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Employee-Tracker
-Employee Tracker
+
+## Description 
+
+This application allows the company to keep track of the list of employees, department, roles salary and managers. It is simple to use and to keep updated.
+
+## Demonstration
+
+[CLIQUE HERE AND WATCH THE VIDEO](https://drive.google.com/file/d/1KV8Eu_oxWzzBReDIt19yyOj9o_XTJUwH/view?usp=share_link)
+
+## Instalation
+
+NODE.JS is required to run the application.
+
+## Credits
+
+Lucas Araujo
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,28 @@
 USE database_db;
 
 INSERT INTO department (name)
-VALUES ('Management'), ('Developers'), ('IT'), ('HR'), ('Intern');
+VALUES  ('R&D'), 
+        ('Manufaturing'), 
+        ('IT'), 
+        ('HR'), 
+        ('Logistics');
 
 
 INSERT INTO role (job_title, salary, department_id)
-VALUES ('General Manager', 450000, 1),('Software Developer', 150000, 2), 
-('IT Lead', 120000, 3),('HR Specialist', 450000, 4),
-('Software Developer Intern', 70000, 5);
+VALUES  ('Engineer', 80000, 1),
+        ('Web Developer', 30000, 3), 
+        ('Help Desk', 250000, 3),
+        ('HR Assistant', 700000, 4),
+        ('Panner', 85000, 5);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Mario', 'Ulloa', 1, 1),('Herbie', 'Smith', 2, null),('James', 'Peach', 3, 2),
-('Dwight', 'Howard', 4, null),('Jason', 'Terry', 3, 2),
-('Joe', 'Dirt', 2, 1),('Cheech', 'Chong', 5, null);
+VALUES  ('Neymar', 'Junior', 1, 1),
+        ('Roger', 'Federer', 3, null),
+        ('Lebron', 'James', 2, 2),
+        ('Felipe', 'Massa', 5, null),
+        ('Cristiano', 'Ronaldo', 4, 2),
+        ('Messi', 'Lionel', 3, 1),
+        ('Pedro', 'Araujo', 5, null);
 
 
